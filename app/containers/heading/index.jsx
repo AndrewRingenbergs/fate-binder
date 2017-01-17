660a6c4736567c234d93e0827e8d09d4c8f28e72
amedw <mr.amedw@gmail.com>
2017-01-17 16:24:26 +1100

    Sign out

--- app/containers/heading/index.jsx
+++ app/containers/heading/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { signOut } from '../../reducers/auth/actions';
 
 import css from './heading.css';
 
@@ -16,7 +17,10 @@ export class HeadingComponent extends React.Component {
     const message = 'Roll for Initiative';
     const user = this.props.username || 'Guest';
     const photo = this.props.photo ? <img src={this.props.photo} alt="profile" width="25" height="25" /> : null;
-    const logoutButton = this.props.authenticated ? <button>Logout</button> : null;
+
+    const logoutButton = this.props.authenticated ?
+      <button onClick={this.props.signOut}>Logout</button> :
+      null;
 
     return (<div className={`pure-menu pure-menu-horizontal ${css.homeMenu}`}>
       <h4 className="pure-menu-heading">{message}</h4>
@@ -33,6 +37,7 @@ HeadingComponent.propTypes = {
   username: React.PropTypes.string,
   authenticated: React.PropTypes.bool.isRequired,
   photo: React.PropTypes.string,
+  signOut: React.PropTypes.func.isRequired,
 };
 
 HeadingComponent.defaultProps = {
@@ -40,4 +45,4 @@ HeadingComponent.defaultProps = {
   photo: null,
 };
 
-export default connect(mapStateToProps)(HeadingComponent);
+export default connect(mapStateToProps, { signOut })(HeadingComponent);
--- app/containers/heading/index.test.jsx
+++ app/containers/heading/index.test.jsx
@@ -2,32 +2,43 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { HeadingComponent } from './index';
 
+const NOOP = () => {};
+
 describe('<Heading />', ()=>{
   test('should contain the name of the application', ()=>{
-    const app = shallow(<HeadingComponent authenticated={false} />);
+    const app = shallow(<HeadingComponent authenticated={false} signOut={NOOP} />);
     expect(app.text()).toContain('Roll for Initiative');
   });
 
   describe('when user is logged in', () => {
     test('should contain the username', ()=>{
       const name = 'Eve';
-      const app = shallow(<HeadingComponent authenticated={true} username={name}/>);
+      const app = shallow(<HeadingComponent authenticated={true} username={name} signOut={NOOP}/>);
 
       expect(app.text()).toContain(name);
     });
 
     test('should contain a logout button', ()=>{
       const name = 'Eve';
-      const app = shallow(<HeadingComponent authenticated={true} username={name}/>);
+      const app = shallow(<HeadingComponent authenticated={true} username={name} signOut={NOOP}/>);
 
       const buttons = app.find('button')
 
       expect(buttons).toHaveLength(1);
       expect(buttons.text()).toBe('Logout');
     });
+
+    test('should call signOut when button is clicked', () => {
+      const signOut = jest.fn();
+      const app = shallow(<HeadingComponent authenticated={true} username={null} signOut={signOut}/>);
+
+      app.find('button').simulate('click');
+
+      expect(signOut.mock.calls).toHaveLength(1);
+    });
   });
   describe('when user is not logged in', () => {
-    const app = shallow(<HeadingComponent authenticated={false} username={null}/>);
+    const app = shallow(<HeadingComponent authenticated={false} username={null} signOut={NOOP}/>);
 
     test('should contain "Guest"', ()=>{
       expect(app.text()).toContain('Guest');
@@ -37,5 +48,6 @@ describe('<Heading />', ()=>{
       const buttons = app.find('button')
       expect(buttons).toHaveLength(0);
     });
+
   })
 });
